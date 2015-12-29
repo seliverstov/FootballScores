e6af7d81cd157c2aa845cc8f6ef83aa23793c1e2
Alexander Seliverstov <seliverstov.a@gmail.com>
2015-12-29 14:19:59 +0300

    Delete unsed drawables

--- app/src/main/java/barqsoft/footballscores/Utilies.java
+++ app/src/main/java/barqsoft/footballscores/Utilies.java
@@ -5,27 +5,11 @@ import android.content.Context;
 /**
  * Created by yehya khaled on 3/3/2015.
  */
-public class Utilies
-{
-    public static final int SERIE_A = 357;
-    public static final int PREMIER_LEGAUE = 354;
+public class Utilies{
+
     public static final int CHAMPIONS_LEAGUE = 362;
-    public static final int PRIMERA_DIVISION = 358;
-    public static final int BUNDESLIGA = 351;
-    public static String getLeague(int leagueNum, Context context)
-    {
-        switch (leagueNum)
-        {
-            case SERIE_A : return context.getString(R.string.seriaa);
-            case PREMIER_LEGAUE : return context.getString(R.string.premierleague);
-            case CHAMPIONS_LEAGUE : return context.getString(R.string.champions_league);
-            case PRIMERA_DIVISION : return context.getString(R.string.primeradivison);
-            case BUNDESLIGA : return context.getString(R.string.bundesliga);
-            default: return context.getString(R.string.league_not_known);
-        }
-    }
-    public static String getMatchDay(int match_day,int league_num, Context context)
-    {
+
+    public static String getMatchDay(int match_day,int league_num, Context context){
         if(league_num == CHAMPIONS_LEAGUE){
             if (match_day <= 6) return context.getString(R.string.match_day_6);
             if (match_day == 7 || match_day == 8) return context.getString(R.string.match_day_7_8);
@@ -34,35 +18,6 @@ public class Utilies
             return context.getString(R.string.match_day_final);
         } else {
             return String.format(context.getString(R.string.mathc_day), match_day);
-        }
-    }
-
-    public static String getScores(int home_goals,int awaygoals)
-    {
-        if(home_goals < 0 || awaygoals < 0) {
-            return " - ";
-        }else{
-            return String.valueOf(home_goals) + " - " + String.valueOf(awaygoals);
-        }
-    }
-
-    public static int getTeamCrestByTeamName (String teamname)
-    {
-        if (teamname==null){return R.drawable.no_icon;}
-        switch (teamname)
-        { //This is the set of icons that are currently in the app. Feel free to find and add more
-            //as you go.
-            case "Arsenal London FC" : return R.drawable.arsenal;
-            case "Manchester United FC" : return R.drawable.manchester_united;
-            case "Swansea City" : return R.drawable.swansea_city_afc;
-            case "Leicester City" : return R.drawable.leicester_city_fc_hd_logo;
-            case "Everton FC" : return R.drawable.everton_fc_logo1;
-            case "West Ham United FC" : return R.drawable.west_ham;
-            case "Tottenham Hotspur FC" : return R.drawable.tottenham_hotspur;
-            case "West Bromwich Albion" : return R.drawable.west_bromwich_albion_hd_logo;
-            case "Sunderland AFC" : return R.drawable.sunderland;
-            case "Stoke City FC" : return R.drawable.stoke_city;
-            default: return R.drawable.no_icon;
         }
     }
 }
