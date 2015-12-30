213a9f214b4704745c4003e69d8836d5b72a2783
Alexander Seliverstov <seliverstov.a@gmail.com>
2015-12-30 19:31:28 +0300

    Fixup

--- app/src/main/java/barqsoft/footballscores/widget/FootballWidgetProvider.java
+++ app/src/main/java/barqsoft/footballscores/widget/FootballWidgetProvider.java
@@ -27,10 +27,10 @@ public class FootballWidgetProvider extends AppWidgetProvider {
         Log.i(TAG, "onUpdate");
         for (int appWidgetId : appWidgetIds) {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-            /*Intent launchIntent = new Intent(context, MainActivity.class);
+            Intent launchIntent = new Intent(context, MainActivity.class);
             launchIntent.putExtra(MainActivity.EXTRA_START_PAGE,MainActivity.DEFAULT_PAGE);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, launchIntent, PendingIntent.FLAG_CANCEL_CURRENT);
-            views.setOnClickPendingIntent(R.id.widget, pendingIntent);*/
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, (int)System.currentTimeMillis(), launchIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
                 setRemoteAdapter(context, views);
